ea36b3c9c5c279f0989c0f8966d45a8123fa569e
Estebanhrze <hectoresteban6972@gmail.com>
2026-07-10 22:15:00 -0500

    configuracion

--- app/core/config.py
+++ app/core/config.py
@@ -2,16 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str
-    APP_VERSION: str
-    DEBUG: bool
+    PROJECT_NAME: str = "Documentex"
+    APP_VERSION: str = "1.0.0"
+    ENVIRONMENT: str = "development"
+    DEBUG: bool = False
 
     SUPABASE_URL: str
     SUPABASE_PUBLISHABLE_KEY: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        extra="ignore",
+        case_sensitive=True
     )
 
 
